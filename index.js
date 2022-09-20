0007839331bc261b886dbba959abaa8764104520
Mykhado <mykhado@macbook-pro-de-mykhado.home>
2022-09-21 00:09:46 +0200

    projet fonctionnel

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ function skynet(max) {
 }
 skynetChoice = skynet(101);
 console.log(skynetChoice);
+let result = document.getElementById("resultat");
+let numberPlayer1 = document.getElementById("selectionNumber1");
+let numberPlayer2 = document.getElementById("selectionNumber2");
+let numberPlayer3 = document.getElementById("selectionNumber3");
+let btnSn = document.getElementById("btnUser");
+let resetBtn = document.getElementById("btnReset");
 
-function getValue() {
-  let numberPlayer = document.querySelector("#numberselect").value;
-  console.log(numberPlayer);
+  
+btnSn.addEventListener("click",()=>{
+  userChoice=Number(numberPlayer1.value*100)+Number(numberPlayer2.value*10)+Number(numberPlayer3.value);
+  console.log(userChoice);
+  
+  if(userChoice<0 ||userChoice> 100|| isNaN(userChoice)){
+  alert(" Attention choisi bien un chiffre entre 0 et 100 !")
+  }
+  if(userChoice<skynetChoice){
+    result.innerHTML= "<p> C'est plus !</p>"
+  }else if( userChoice>skynetChoice){
+    result.innerHTML="<p> C'est moins !</p>"
+  }else if (userChoice= skynetChoice){
+    result.innerHTML="<p> Bravo tu as trouvé, tu as gagné ma reconnaissance eternel !</p>"
+  }
+});
 
-  return numberPlayer;
-}
+resetBtn.addEventListener("click",()=>{
+  location.reload();
+});
+
+
+
+
